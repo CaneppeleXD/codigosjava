5b5bbe3b566b03a10ad3e3af521026447a8fa945
CaneppeleXD <109169622+CaneppeleXD@users.noreply.github.com>
2022-07-27 22:11:29 -0300

    Add files via upload

---
+++ source/NumeroInteiro.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.univates.source;
+
+/**
+ *
+ * @author joao.caneppele
+ */
+public class NumeroInteiro {
+
+    public int valor;
+
+    public NumeroInteiro(int valor) {
+
+        this.valor = valor;
+    }
+
+    public boolean verificarPrimo() {
+        boolean retorno = true;
+        for (int i = 2; retorno == true && i < valor; i++) {
+            if (valor % i == 0) {
+                retorno = false;
+            }
+        }
+
+        return (retorno);
+    }
+
+    public int contarDivisores() {
+        int divisores = 0;
+        for (int i = 1; i <= valor; i++) {
+            if (valor % i == 0) {
+                divisores++;
+            }
+        }
+        return (divisores);
+    }
+    //primo relativo / recever como parametro outro numero inteiro
+
+}
