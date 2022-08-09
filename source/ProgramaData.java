fdb496772a676ee42410e52b63313e01197fc688
Caneppele <caneppele1.6@gmail.com>
2022-08-09 11:54:55 -0300

    programadata

---
+++ source/ProgramaData.java
@@ -0,0 +1,7 @@
+public class ProgramaData {
+    public static void main(String[] args) {
+        Data d = new Data(05, 02, 2000);
+        Data d2 = new Data(28, 06, 2000);
+        System.out.println(d.obterSigno());
+    }
+}
