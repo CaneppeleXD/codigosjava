5adc5da1135822709e2dde02f2df32b0d9b17e02
CaneppeleXD <109169622+CaneppeleXD@users.noreply.github.com>
2022-08-08 22:09:06 -0300

    Add files via upload
    classe data

---
+++ source/Data.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.univates.source;
+
+/**
+ *
+ * @author joao.caneppele
+ */
+public class Data {
+    private int dia;
+    
+    private int mes;
+    
+    private int ano;
+    
+    public Data (int dia, int mes, int ano){
+        trocarDia(dia);
+        trocarMes(mes);
+        trocarAno(ano);
+    }
+    
+    
+    public boolean trocarDia (int dia) {
+        boolean retorno = false;
+        if(dia > 0 && dia <= 32){
+            this.dia = dia;
+            retorno = true;
+        }
+        return(retorno);
+    }
+    
+    public boolean trocarMes (int mes) {
+        boolean retorno = false;
+        if(mes > 0 && mes <= 12){
+            this.mes = mes;
+            retorno = true;
+        }
+        return(retorno);
+    }
+    
+    public boolean trocarAno (int ano) {
+        boolean retorno = false;
+        if(ano > 0){
+            this.ano= ano;
+            retorno = true;
+        }
+        return(retorno);
+    }
+    
+    public int obterDia (){
+        return(dia);
+    }
+    
+    public int obterMes (){
+        return(mes);
+    }
+    
+    public int obterAno (){
+        return(ano);
+    }
+    
+    public boolean ehBissexto (){
+        boolean retorno = false;
+
+        return(retorno);
+    }
+}
