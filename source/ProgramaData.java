5c44fc48bcfe342ff98f85ce20c87ced5aceded8
CaneppeleXD <109169622+CaneppeleXD@users.noreply.github.com>
2022-08-10 21:57:24 -0300

    Update ProgramaData.java

--- source/ProgramaData.java
+++ source/ProgramaData.java
@@ -1,7 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.univates.source;
+
+/**
+ *
+ * @author joao.caneppele
+ */
 public class ProgramaData {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         Data d = new Data(05, 02, 2000);
-        Data d2 = new Data(28, 06, 2000);
-        System.out.println(d2.formatarData("mm/yy/dd"));
+        Data d2 = new Data(11, 11, 11);
+        System.out.println(d2.ehPalindromo());
+        
     }
+    
 }
