f868629126cb1dbd56549484d4c5498cd7c33ef7
caneppele1.6@gmail.com <caneppele1.6@gmail.com>
2022-08-18 14:43:25 -0300

    pos triangulo

--- source/ProgramaData.java
+++ source/ProgramaData.java
@@ -15,10 +15,8 @@ public class ProgramaData {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Data d = new Data(05, 02, 2000);
-        Data d2 = new Data(11, 8, 2022);
-        System.out.println(d2.obterDiaSemana());
-        
+        Triangulo t = new Triangulo(15, 1, 18);
+        System.out.println(t.getA());
     }
     
 }
--- source/Triangulo.java
+++ source/Triangulo.java
@@ -1,9 +1,84 @@
-package br.univates.source;
+//package br.univates.source;
 
 public class Triangulo {
-    double a;
-    double b;
-    double c;
+    private double a;
+    private double b;
+    private double c;
 
-    
+    public Triangulo(double a, double b, double c) {
+        this.a = 1;
+        this.b = 1;
+        this.c = 1;
+        if (Triangulo.ehValido(a, b, c)) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+
+    public boolean setA(double a) {
+        boolean retorno = Triangulo.ehValido(a, b, c);
+        if (retorno)
+            this.a = a;
+        return (retorno);
+    }
+
+    public boolean setB(double b) {
+        boolean retorno = Triangulo.ehValido(a, b, c);
+        if (retorno)
+            this.b = b;
+        return (retorno);
+    }
+
+    public boolean setC(double c) {
+        boolean retorno = Triangulo.ehValido(a, b, c);
+        if (retorno)
+            this.c = c;
+        return (retorno);
+    }
+
+    public boolean ehValido() {
+        return (Math.abs(a - b) < c && c < Math.abs(a + b));
+    }
+
+    public static boolean ehValido(double a, double b, double c) {
+        return (Math.abs(a - b) < c && c < Math.abs(a + b));
+    }
+
+    public double getA() {
+        return this.a;
+    }
+
+    public double getB() {
+        return this.b;
+    }
+
+    public double getC() {
+        return this.c;
+    }
+
+    public double getPerimetro() {
+        return (a + b + c);
+    }
+
+    public double getArea() {
+        double p = getPerimetro() / 2;
+        return (Math.sqrt(p * (p - a) * (p - b) * (p - c)));
+    }
+
+    public double getAltura(double base) {
+        return (2 * getArea() / base);
+    }
+
+    public void ampliarArestas(double aux) {
+        this.a = this.a * aux;
+        this.b = this.b * aux;
+        this.c = this.c * aux;
+    }
+
+    public void reduzirArestas(double aux) {
+        this.a = this.a / aux;
+        this.b = this.b / aux;
+        this.c = this.c / aux;
+    }
 }
