5a617987b485efdfbdabc555b2f466f422bf58cd
CaneppeleXD <109169622+CaneppeleXD@users.noreply.github.com>
2022-08-17 21:45:10 -0300

    Update Dado.java

--- source/Dado.java
+++ source/Dado.java
@@ -7,7 +7,7 @@ public class Dado {
         historico = new int[0];
     }
 
-    private void adicionaEvento(int evento) {
+    private static int[] adicionaEvento(int[] historico, int evento) {
         int[] memoria = new int[historico.length];
         for (int i = 0; i < memoria.length; i++) {
             memoria[i] = historico[i];
@@ -17,11 +17,12 @@ public class Dado {
             historico[i] = memoria[i];
         }
         historico[memoria.length] = evento;
+        return(historico);
     }
 
     public int girarDado() {
         int evento = (int) Math.round(Math.random() * 6);
-        adicionaEvento(evento);
+        historico = Dado.adicionaEvento(historico,evento);
         return (evento);
     }
 
@@ -32,4 +33,22 @@ public class Dado {
         }
         return (retorno);
     }
+
+    public int getMedia(){
+        return (int) Util.calcularMedia(historico);
+    }
+
+    public int getModa(){
+        return(int) Util.encontrarModa(historico);
+    }
+
+    public int getUltimo(){
+        return(historico[historico.length-1]);
+    }
+
+    public int getDesvioPadrao(){
+        return (int) Util.calcularDesvioPadrao(historico);
+    }
+
+    
 }
